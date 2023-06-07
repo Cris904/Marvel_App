18874a48e013aa98acdc85176c6ac19f81de2df0
Cris904 <cristiafl3@gmail.com>
2023-06-07 01:25:45 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const ejs = require('ejs');
+
+// Conecta con la base de datos MongoDB
+const uri = 'mongodb://127.0.0.1:27017'; // URI de conexión de MongoDB, modificar según la configuración
+const client = new MongoClient(uri);
+
+app.set('view engine', 'ejs');
+
+// Ruta para mostrar la lista de comics
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+
+    const db = client.db('marvel'); // Nombre de la base de datos
+    const comicsCollection = db.collection('comics'); // Nombre de la colección
+
+    // Consulta los registros de comics ordenados alfabéticamente por título
+    const comics = await comicsCollection.find().sort({ title: 1 }).toArray();
+
+    res.render('index', { comics });
+  } catch (error) {
+    console.error('Ha ocurrido un error:', error);
+    res.status(500).send('Error al obtener los datos de la base de datos.');
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en http://localhost:3000');
+});
---
+++ comics.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const { MongoClient } = require('mongodb');
+
+async function getDataAndSaveToMongoDB() {
+  try {
+    // Realiza la solicitud a la API de Marvel
+    const ts = 1;
+    const private_key = '90109acfcbf8e95ed83855ccfdc3537227a64757';
+    const public_key = '42c8bab782fd9cce43750fcedbf0ea84';
+    const hashed = 'e0c2665973311914ddabd2b51421778a';
+
+    const url = `https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${public_key}&hash=${hashed}`;
+
+    const response = await axios.get(url);
+    const data = response.data.data.results;
+
+    // Conecta con la base de datos MongoDB
+    const uri = 'mongodb://127.0.0.1:27017'; // URI de conexión de MongoDB, modificar según la configuración
+    const client = new MongoClient(uri);
+    await client.connect();
+
+    const db = client.db('marvel'); // Nombre de la base de datos
+    const comicsCollection = db.collection('comics'); // Nombre de la colección
+
+    // Guarda los datos en la colección "comics"
+    await comicsCollection.insertMany(data);
+
+    // Cierra la conexión con la base de datos
+    await client.close();
+
+    console.log('Los datos se han guardado en la base de datos MongoDB correctamente.');
+  } catch (error) {
+    console.error('Ha ocurrido un error:', error);
+  }
+}
+
+// Ejecuta la función principal
+getDataAndSaveToMongoDB();
