5e87bffcf178d6c88db41606ab6103122a2cfc07
Cris904 <cristiafl3@gmail.com>
2023-06-07 20:25:16 -0500

    api2

--- app.js
+++ app.js
@@ -4,22 +4,18 @@ const ejs = require('ejs');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Conecta con la base de datos MongoDB
-const uri = process.env.MONGODB_URI; // La URL de conexión a MongoDB se configurará en Render
+const uri = process.env.MONGODB_URI; // La URL de conexión de MongoDB se configurará en Render
 const client = new MongoClient(uri);
 
 app.set('view engine', 'ejs');
 
-// Ruta para mostrar la lista de cómics
 app.get('/', async (req, res) => {
   try {
     await client.connect();
 
-    const db = client.db(); // No es necesario especificar el nombre de la base de datos
-    const comicsCollection = db.collection('comics'); // Nombre de la colección
+    const db = client.db('marvel');
+    const comicsCollection = db.collection('comics');
 
-    // Consulta los registros de cómics ordenados alfabéticamente por título
     const comics = await comicsCollection.find().sort({ title: 1 }).toArray();
 
     res.render('index', { comics });
