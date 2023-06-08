dc3e11658b09352e878842bf9d440a69713b38f5
analista.desarrollo <analista.desarrollo@etib.com.co>
2023-06-08 07:45:59 -0500

    cambios app.js

--- app.js
+++ app.js
@@ -9,11 +9,29 @@ const client = new MongoClient(uri);
 
 app.set('view engine', 'ejs');
 
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+
+async function closeDatabaseConnection() {
+  try {
+    await client.close();
+    console.log('MongoDB connection closed');
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+    throw error;
+  }
+}
+
 // Ruta para mostrar la lista de comics
 app.get('/', async (req, res) => {
   try {
-    await client.connect();
-
     const db = client.db('marvel');
     const comicsCollection = db.collection('comics');
 
@@ -23,8 +41,6 @@ app.get('/', async (req, res) => {
   } catch (error) {
     console.error('Ha ocurrido un error:', error);
     res.status(500).send('Error al obtener los datos de la base de datos.');
-  } finally {
-    await client.close();
   }
 });
 
@@ -34,6 +50,15 @@ app.get('/app-ip', (req, res) => {
   res.send(`App IP: ${appIp}`);
 });
 
-app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
-});
+async function startServer() {
+  try {
+    await connectToDatabase();
+    app.listen(port, () => {
+      console.log(`Servidor iniciado en http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
+}
+
+startServer().catch(console.dir);
