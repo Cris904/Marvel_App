0c83ca7a867837314e2f535ea667c5a3fa5400ba
Cris904 <cristiafl3@gmail.com>
2023-06-07 20:10:59 -0500

    api1

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 const express = require('express');
-const app = express();
 const { MongoClient } = require('mongodb');
 const ejs = require('ejs');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
 // Conecta con la base de datos MongoDB
-const uri = 'mongodb+srv://cristianflo7:Zhongkui24@atlascluster.lxduy2m.mongodb.net/?retryWrites=true&w=majority'; // URI de conexión de MongoDB, modificar según la configuración
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const uri = process.env.MONGODB_URI; // La URL de conexión a MongoDB se configurará en Render
+const client = new MongoClient(uri);
 
 app.set('view engine', 'ejs');
 
-// Ruta para mostrar la lista de comics
+// Ruta para mostrar la lista de cómics
 app.get('/', async (req, res) => {
   try {
     await client.connect();
 
-    const db = client.db('marvel'); // Nombre de la base de datos
+    const db = client.db(); // No es necesario especificar el nombre de la base de datos
     const comicsCollection = db.collection('comics'); // Nombre de la colección
 
-    // Consulta los registros de comics ordenados alfabéticamente por título
+    // Consulta los registros de cómics ordenados alfabéticamente por título
     const comics = await comicsCollection.find().sort({ title: 1 }).toArray();
 
     res.render('index', { comics });
@@ -35,6 +31,6 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
 });
